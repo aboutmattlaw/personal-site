52a40098c9539baf23af288cf25bfd4461920252
Matt <aboutmattlaw@gmail.com>
2023-12-11 22:35:17 -0500

    adds job search page and js change to support

--- js/script2.js
+++ js/script2.js
@@ -12,6 +12,19 @@ function gotoGoogle(e){
 
 
 
+document.querySelector(".form").addEventListener("submit", gotoGoogleJob);       
+function gotoGoogleJob(e){
+    e.preventDefault()
+    console.log(e)
+    console.log(e.target.term.value)
+    let term = e.target.term.value
+    const searchUrl = `https://www.google.com/search?q=${term}+site%3Ahttps%3A%2F%2Fgreenhouse.iom%2F+OR+site%3Ahttps%3A%2F%2FMyworkdayjobs.com%2F+OR+site%3Ahttps%3A%2F%2Fapply.workable.com%2F+OR+site%3Ahttps%3A%2F%2Flever.co`
+    console.log(searchUrl)
+    window.location.href=searchUrl
+}
+
+
+
 
 const time = new Date().getHours();
 let greeting;
