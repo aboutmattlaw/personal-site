851b85d2acde0c177dece8fa5c0def699c177440
Matt <aboutmattlaw@gmail.com>
2024-05-06 12:02:36 -0400

    removed links

--- js/script2.js
+++ js/script2.js
@@ -18,9 +18,9 @@ function gotoGoogleJob(e){
     console.log(e)
     console.log(e.target.term.value)
     let term = e.target.term.value
-    const searchUrl = `https://www.google.com/search?q=${term}+site%3Ahttps%3A%2F%2Fgreenhouse.iom%2F+OR+site%3Ahttps%3A%2F%2FMyworkdayjobs.com%2F+OR+site%3Ahttps%3A%2F%2Fapply.workable.com%2F+OR+site%3Ahttps%3A%2F%2Flever.co`
-    console.log(searchUrl)
-    window.location.href=searchUrl
+    const searchUrlJobs = `https://www.google.com/search?q=${term}+site%3Ahttps%3A%2F%2Fgreenhouse.iom%2F+OR+site%3Ahttps%3A%2F%2FMyworkdayjobs.com%2F+OR+site%3Ahttps%3A%2F%2Fapply.workable.com%2F+OR+site%3Ahttps%3A%2F%2Flever.co`
+    console.log(searchUrlJobs)
+    window.location.href=searchUrlJobs
 }
 
 
